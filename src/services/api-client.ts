9947556e49ab5d2a88fad8243cdedc946a032e5d
Saied Sani <saied.sani.biderooni@gmail.com>
2023-06-09 16:55:41 +0200

    Fetching_Games with React-Query

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,8 +1,12 @@
 import axios from "axios";
+export interface FetchResponse<T> {
+  count: number;
+  results: T[];
+}
 
 export default axios.create({
-    baseURL:'https://api.rawg.io/api',
-    params:{
-        key:'d64a3d3fd14a4269b12057d029a7916e'
-    }
-})
+  baseURL: "https://api.rawg.io/api",
+  params: {
+    key: "d64a3d3fd14a4269b12057d029a7916e",
+  },
+});
