84721fe68c412532cc29d4beb767942d2765ce33
Saied Sani <saied.sani.biderooni@gmail.com>
2023-06-13 20:53:13 +0200

    Refractor mange state with zustand

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,14 +2,12 @@ import { HStack, Image } from "@chakra-ui/react";
 import logo from "../assets/logo.webp";
 import ColorModeSwitch from "./ColorModeSwitch";
 import SearchInput from "./SearchInput";
-interface Props {
-  onSerach: (searchText: string) => void;
-}
-const NavBar = ({ onSerach }: Props) => {
+
+const NavBar = () => {
   return (
     <HStack padding="10px">
       <Image src={logo} boxSize="60px" />
-      <SearchInput onSerach={onSerach} />
+      <SearchInput />
       <ColorModeSwitch />
     </HStack>
   );
--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,18 +1,16 @@
 import { Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
 import { useRef } from "react";
 import { BsSearch } from "react-icons/bs";
+import useGameQueryStore from "../store";
 
-interface Props {
-  onSerach: (searchText: string) => void;
-}
-
-const SearchInput = ({ onSerach }: Props) => {
+const SearchInput = () => {
   const ref = useRef<HTMLInputElement>(null);
+  const setSearchText = useGameQueryStore((s) => s.setSearchText);
   return (
     <form
       onSubmit={(event) => {
         event.preventDefault();
-        if (ref.current) onSerach(ref.current.value);
+        if (ref.current) setSearchText(ref.current.value);
       }}
     >
       <InputGroup>
